09d3ff8d7e13eff9685711a26d57855cb609fce4
Dilawar Mahmood <dilawarmm@outlook.com>
2025-03-15 18:08:53 +0100

    split out parser test and rename test files

--- tests/test_poly_diff.py
+++ tests/test_poly_diff.py
@@ -1,13 +1,4 @@
 from symdiff.core import differentiate
-from symdiff.expressions import (
-    Constant,
-    Negation,
-    Power,
-    Product,
-    Sum,
-    Variable,
-)
-from symdiff.parser import parse_expression
 
 
 def test_constant_differentiation():
@@ -103,45 +94,3 @@ def test_negative_exponents():
     assert str(differentiate("2*x^-1")) == "-2*x^-2"
     assert str(differentiate("x^-3 + x^2")) == "-3*x^-4 + 2*x"
     assert str(differentiate("x^-0.5")) == "-0.5*x^-1.5"
-
-
-def test_parser():
-    """Test the expression parser"""
-
-    assert isinstance(parse_expression("5"), Constant)
-    assert str(parse_expression("5")) == "5"
-
-    assert isinstance(parse_expression("x"), Variable)
-    assert str(parse_expression("x")) == "x"
-
-    assert isinstance(parse_expression("x^2"), Power)
-    assert str(parse_expression("x^2")) == "x^2"
-
-    assert isinstance(parse_expression("2*x"), Product)
-    assert str(parse_expression("2*x")) == "2*x"
-
-    assert isinstance(parse_expression("x + 1"), Sum)
-    assert str(parse_expression("x + 1")) == "x + 1"
-
-    assert isinstance(parse_expression("-x"), Negation)
-    assert str(parse_expression("-x")) == "-x"
-
-    assert str(parse_expression("x - 1")) == "x + -1"
-
-    expr = parse_expression("x^2 + 2*x + 1")
-    assert isinstance(expr, Sum)
-    assert len(expr.terms) == 3
-    assert str(expr) == "x^2 + 2*x + 1"
-
-    expr = parse_expression("x^2 - 2*x + 1")
-    assert isinstance(expr, Sum)
-    assert len(expr.terms) == 3
-    assert str(expr) == "x^2 + -2*x + 1"
-
-    expr = parse_expression("x^-1")
-    assert isinstance(expr, Power)
-    assert str(expr) == "x^-1"
-
-    expr = parse_expression("x^-2 + 3*x^-1")
-    assert isinstance(expr, Sum)
-    assert str(expr) == "x^-2 + 3*x^-1"
